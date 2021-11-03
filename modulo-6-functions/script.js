de0ad84e9a18dfbc8eac32030f925601f60e1a98
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-11-03 17:49:36 -0300

    commit

---
+++ modulo-6-functions/script.js
@@ -0,0 +1,72 @@
+console.log('----------functions----------')
+
+const sum = function (num1, num2) {
+    let total = num1 + num2
+    return total;
+}
+
+
+let num1 = 34
+let num2 = 25
+sum(num1, num2)
+
+console.log(`o numero 1 é: ${num1}`)
+console.log(`o numero 2 é: ${num2}`)
+console.log(`A soma é ${sum(num1, num2)}`)
+
+console.log('----------function scope----------')
+
+let subject
+
+function createThink() {
+    subject = 'study'
+}
+
+console.log(subject)
+console.log(createThink(subject))
+console.log(subject)
+
+console.log('----------function hoisting----------')
+
+sayMyName()
+
+function sayMyName() {
+    console.log('Lucas')
+}
+
+console.log('----------arrow function----------')
+
+const myName = (name) => {
+    console.log(name)
+}
+
+myName('lucas =>')
+
+console.log('----------callback function----------')
+
+function sayMyOS(os) {
+    console.log('antes de executar a callback')
+    os()
+    console.log('depois de executar a callback')
+
+}
+
+sayMyOS(
+    () => {
+        console.log('[estou em uma callback]')
+    }
+)
+
+
+console.log('----------function constructor----------')
+function Person(name) {
+    this.name = name
+    this.walk = function(){
+        return this.name + " está andando"
+    }
+}
+const lucas = new Person("lucas")
+const joao = new Person("joao")
+
+console.log(lucas.walk())
+console.log(joao.walk())
---
+++ modulo-7-datamanipulation/script.js
@@ -0,0 +1,7 @@
+console.log('-------prototype-------')
+
+let protoString = "lucas".__proto__
+let protoNum = 66.5.__proto__
+
+console.log(protoString)
+console.log(protoNum)

307c948637a7d99f95fbe2985a3690bb08a244fb
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-11-04 17:02:55 -0300

    commit modulo 8 on work

---
+++ modulo-8-expressoes&operadores/script.js
@@ -0,0 +1,48 @@
+console.log('----------Expressões e Operadores----------')
+
+
+console.log('- expressão new tem a função de criar um objeto')
+
+let nome = new String('lucas')
+let age = new Number(19)
+let date = new Date('2021-01-01')
+
+
+console.log(nome, age, date)
+
+console.log('')
+
+console.log('- operador unario (typeof e delete)')
+
+console.log("- typeof:")
+console.log(typeof "lucas")
+
+const person = {
+    nome: "lucas",
+    age: 19
+}
+
+console.log(person)
+
+delete person.age
+console.log("- delete:")
+console.log(person)
+
+
+console.log('')
+console.log('- operador exponencial')
+console.log('**')
+
+console.log(2 ** 4)
+
+
+console.log("- operadores de comparação estritamente igual e estritamente diferente")
+
+let one = 1
+let two = 2
+
+console.log(one === "1")
+console.log(one === 1)
+
+
+
